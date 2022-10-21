dca392fceaa4aea26887a362ee4a0abde99f1e68
Tonio Loewald <tonio@loewald.com>
2022-10-20 21:10:05 -0700

    [new] make-error

---
+++ src/make-error.ts
@@ -0,0 +1,9 @@
+const stringify = (x: any) => {
+  try {
+    return JSON.stringify(x)
+  } catch(_) {
+    return '{has circular references}'
+  }
+}
+
+export const makeError = (...messages: any[]) => new Error(messages.map(stringify).join(' '))
