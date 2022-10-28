e94c3c2a0ca44009b898c799328cafbd702aad54
Tonio Loewald <tonio@loewald.com>
2022-10-28 11:03:08 -0700

    [new] refactored bindings into its own file [fix] list-binding correctly reusing elements [new] style binding

--- src/bind.ts
+++ src/bind.ts
@@ -1,49 +1,13 @@
 import { xin, touch, observe, observerShouldBeRemoved } from './xin'
-import { getListBinding } from './list-binding'
 import { XinObject, XinTouchableType } from './xin-types'
+import { XinBinding, bindings } from './bindings'
 
-export type XinBinding = {
-  toDOM: (element: HTMLElement, value: any, options?: XinObject) => void,
-  fromDOM?: (element: HTMLElement) => any
-}
-
-export const bindings: {[key: string | symbol] : XinBinding} = {
-  value: {
-    toDOM(element: HTMLElement, value: any, options?: XinObject) {
-      // @ts-expect-error
-      if(element.value !== undefined) {
-        // @ts-expect-error
-        element.value = value
-      } else {
-        throw new Error(`cannot set value of <${element.tagName}>`)
-      }
-    },
-
-    fromDOM(element: HTMLElement) {
-      // @ts-expect-error
-      return element.value
-    }
-  },
-
-  text: {
-    toDOM(element: HTMLElement, value: any) {
-      element.textContent = value
-    }
-  },
-
-  list: {
-    toDOM(element: HTMLElement, value: any[], options?: XinObject) {
-      console.log({options})
-      const {bindInstance} = options || {}
-      const listBinding = getListBinding(element, bindInstance)
-      // @ts-expect-error
-      listBinding.update(value._xinValue || value)
-    }
-  }
-}
 
 export const bind = (element: HTMLElement, what: XinTouchableType, binding: XinBinding, options?: XinObject) => {
   const {toDOM, fromDOM} = binding
+  if (!what || (typeof what === 'object' && !what._xinPath)) {
+    throw new Error('bind requires a path or object with xin Proxy')
+  }
   const path = typeof what === 'string' ? what : what._xinPath
   if (toDOM) {
     toDOM(element, xin[path], options)
