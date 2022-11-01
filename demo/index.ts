296bd5f637a9e8f8e083f41bfe193693e5cead79
Tonio Loewald <tonio@loewald.com>
2022-11-01 08:59:47 -0700

    [new] component.ts brought over [fix] removed spam from bindings

--- demo/index.ts
+++ demo/index.ts
@@ -3,10 +3,13 @@ import {bind} from '../src/bind'
 import {bindings} from '../src/bindings'
 import {hotReload} from '../src/hot-reload'
 import {elements} from '../src/elements'
+import {makeWebComponent} from '../src/components'
 
 /* global window, document */
 
 console.time('total')
+
+const {fragment, h1, div, input, template, button, span, label, slot, style} = elements
 
 const randomColor = () => `hsl(${Math.floor(Math.random() * 360)} ${Math.floor(Math.random() * 4 + 1) * 25}% 50%)`
 
@@ -33,6 +36,46 @@ const makeItems = (howMany) => {
   return items
 }
 
+const testComponent = makeWebComponent('test-component', {
+  content: [
+    elements.h3('top'),
+    elements.slot(),
+    elements.div('bottom', {dataRef: 'foo'})
+  ],
+  methods: {
+    render() {
+      console.log(this.elementRefs.foo.textContent)
+    } 
+  }
+})
+
+const appLayout = makeWebComponent('app-layout', {
+  style: {
+    ':host': {
+      display: 'flex',
+      flexDirection: 'column'
+    },
+    ':host .body': {
+      display: 'flex',
+      flex: '1 1 auto',
+      width: '100%'
+    },
+    '::slotted(:not([slot]))': {
+      flex: '1 1 auto'
+    }
+  },
+  content: [
+    slot({name: 'header'}),
+    div(
+      { class: 'body' },
+      slot({name: 'left'}),
+      slot(),
+      slot({name: 'right'})
+    ),
+    slot({name: 'footer'})
+  ]
+})
+
 xin.app = {
   title: 'xinjs binding test',
   itemsToCreate: INITIAL_ITEMS,
@@ -45,8 +88,9 @@ Object.assign(window, {
   xin
 })
 
-const {fragment, h1, div, input, template, button, span, label} = elements
 const colorConversionSpan = document.createElement('span')
+
+document.head.append(style('body { font-family: Sans-serif }'))
 
 document.body.append(fragment(
   bind(h1(), 'app.title', bindings.text ),
@@ -108,23 +152,25 @@ document.body.append(fragment(
       console.timeEnd('scramble and modify')
     }})
   ),
-  bind(div(template(span({style: {
+  bind(
+    div(template(span({style: {
       display: 'inline-block',
       padding: '10px',
       margin: '5px',
       fontFamily: 'monospace',
       width: '200px'
     }}))), xin.app.items, bindings.list, {
-    bindInstance(element, obj) {
-      colorConversionSpan.style.color = obj.color
-      const color = colorConversionSpan.style.color
-      if (element.style.color !== color) {
-        element.style.border = `1px solid ${color}`
-        element.style.color = color
-        element.textContent = `${obj.id} ${obj.color}`
+      bindInstance(element, obj) {
+        colorConversionSpan.style.color = obj.color
+        const color = colorConversionSpan.style.color
+        if (element.style.color !== color) {
+          element.style.border = `1px solid ${color}`
+          element.style.color = color
+          element.textContent = `${obj.id} ${obj.color}`
+        }
       }
     }
-  }),
+  ),
   bind(div(span({style: {
     display: 'inline-block',
     padding: '10px',
