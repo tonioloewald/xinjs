18ea48fb285683bfe54cb86045d68252ecb398fc
Tonio Loewald <tonio@loewald.com>
2022-11-04 14:39:45 -0700

    [fix] remove build after merge

--- build/bind.d.ts
+++
@@ -1,3 +0,0 @@
-import { XinObject, XinTouchableType, XinBinding } from './xin-types';
-export declare const bind: (element: HTMLElement, what: XinTouchableType, binding: XinBinding, options?: XinObject) => HTMLElement;
-//# sourceMappingURL=bind.d.ts.map
--- build/by-path.d.ts
+++
@@ -1,9 +0,0 @@
-import { XinObject } from './xin-types';
-declare type Part = string | string[];
-declare type PartArray = Part[];
-declare function pathParts(path: string | PartArray): PartArray;
-declare function getByPath(obj: XinObject, path: string): any;
-declare function setByPath(orig: XinObject, path: string, val: any): boolean;
-declare function deleteByPath(orig: XinObject, path: string): void;
-export { getByPath, setByPath, deleteByPath, pathParts };
-//# sourceMappingURL=by-path.d.ts.map
