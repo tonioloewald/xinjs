cd1a1ac00a5720ff603c52d441ef09af2439e333
Tonio Loewald <tonio@loewald.com>
2023-04-09 01:27:59 +0300

    Update elements.ts
    [fix] element properties are composed and applied after all children are added—fixes issue with select not being able to have value set until it has option with value

--- src/elements.ts
+++ src/elements.ts
@@ -1,6 +1,6 @@
 import { bind, on } from './bind'
 import { bindings } from './bindings'
-import { ElementPart, ElementCreator, SwissArmyElement } from '../src/xin-types'
+import { ElementPart, ElementProps, ElementCreator, SwissArmyElement } from '../src/xin-types'
 import { camelToKabob } from './string-case'
 
 const templates: { [key: string]: HTMLElement } = {}
@@ -14,6 +14,7 @@ export const create = (tagType: string, ...contents: ElementPart[]): SwissArmyEl
     templates[tagType] = globalThis.document.createElement(tagType)
   }
   const elt = templates[tagType].cloneNode() as SwissArmyElement
+  const elementProps: ElementProps = {}
   for (const item of contents) {
     if (item instanceof Element || item instanceof DocumentFragment || typeof item === 'string' || typeof item === 'number') {
       if (elt instanceof HTMLTemplateElement) {
@@ -22,51 +23,52 @@ export const create = (tagType: string, ...contents: ElementPart[]): SwissArmyEl
         elt.append(item as Node)
       }
     } else {
-      for (const key of Object.keys(item)) {
-        const value: any = (item)[key]
-        if (key === 'apply') {
-          value(elt)
-        } else if (key === 'style') {
-          if (typeof value === 'object') {
-            for (const prop of Object.keys(value)) {
-              if (prop.startsWith('--')) {
-                elt.style.setProperty(prop, value[prop])
-              } else {
-                // @ts-expect-error
-                elt.style[prop] = value[prop]
-              }
-            }
+      Object.assign(elementProps, item)
+    }
+  }
+  for (const key of Object.keys(elementProps)) {
+    const value: any = elementProps[key]
+    if (key === 'apply') {
+      value(elt)
+    } else if (key === 'style') {
+      if (typeof value === 'object') {
+        for (const prop of Object.keys(value)) {
+          if (prop.startsWith('--')) {
+            elt.style.setProperty(prop, value[prop])
           } else {
-            elt.setAttribute('style', value)
-          }
-        } else if (key.match(/^on[A-Z]/) != null) {
-          const eventType = key.substring(2).toLowerCase()
-          on(elt, eventType, value)
-        } else if (key.match(/^bind[A-Z]/) != null) {
-          const bindingType = key.substring(4, 5).toLowerCase() + key.substring(5)
-          const binding = bindings[bindingType]
-          if (binding !== undefined) {
-            bind(elt, value, binding)
-          } else {
-            throw new Error(`${key} is not allowed, bindings.${bindingType} is not defined`)
-          }
-        } else {
-          const attr = camelToKabob(key)
-
-          if (attr === 'class') {
-            value.split(' ').forEach((className: string) => {
-              elt.classList.add(className)
-            })
-          // @ts-expect-error-error
-          } else if (elt[attr] !== undefined) {
-            // @ts-expect-error-error
-            elt[attr] = value
-          } else if (typeof value === 'boolean') {
-            value ? elt.setAttribute(attr, '') : elt.removeAttribute(attr)
-          } else {
-            elt.setAttribute(attr, value)
+            // @ts-expect-error
+            elt.style[prop] = value[prop]
           }
         }
+      } else {
+        elt.setAttribute('style', value)
+      }
+    } else if (key.match(/^on[A-Z]/) != null) {
+      const eventType = key.substring(2).toLowerCase()
+      on(elt, eventType, value)
+    } else if (key.match(/^bind[A-Z]/) != null) {
+      const bindingType = key.substring(4, 5).toLowerCase() + key.substring(5)
+      const binding = bindings[bindingType]
+      if (binding !== undefined) {
+        bind(elt, value, binding)
+      } else {
+        throw new Error(`${key} is not allowed, bindings.${bindingType} is not defined`)
+      }
+    } else {
+      const attr = camelToKabob(key)
+
+      if (attr === 'class') {
+        value.split(' ').forEach((className: string) => {
+          elt.classList.add(className)
+        })
+      // @ts-expect-error-error
+      } else if (elt[attr] !== undefined) {
+        // @ts-expect-error-error
+        elt[attr] = value
+      } else if (typeof value === 'boolean') {
+        value ? elt.setAttribute(attr, '') : elt.removeAttribute(attr)
+      } else {
+        elt.setAttribute(attr, value)
       }
     }
   }
